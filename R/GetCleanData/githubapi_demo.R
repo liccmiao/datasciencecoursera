49f42cce2fa1f3eca22878fe9e7acbfa41a40639
Claude Lee <liccbetter@gmail.com>
2015-02-13 18:45:11 +0800

    gettingandcleaningdata/githubapi_demo
    add dir GetCleanData for course getting and cleanning data (getdata-id)
    add demo for githubapi use in httr pkg

---
+++ R/GetCleanData/githubapi_demo.R
@@ -0,0 +1,22 @@
+library(httr)
+library(httpuv)
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. Register an application at https://github.com/settings/applications;
+#    Use any URL you would like for the homepage URL (http://github.com is fine)
+#    and http://localhost:1410 as the callback url
+#
+#    Insert your client ID and secret below - if secret is omitted, it will
+#    look it up in the GITHUB_CONSUMER_SECRET environmental variable.
+myapp <- oauth_app("getcleandataquiz2app", "xxxxxxxxxxxxxxxxxxxx", secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/repos/jtleek/datasharing", gtoken)
+stop_for_status(req)
+content(req)$created_at
